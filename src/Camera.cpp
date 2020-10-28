914f93f3ad2c1c135d0be74ea71f5c75fd2c319c
Valentine <valentinezekiev@gmail.com>
2020-10-28 13:19:37 +0100

    ROB-17 added a new node and some documentation, also setup the camera to work for my setup. Will look into whatever the hell docker is. Should be more or less done by now tho.

---
+++ src/Camera.cpp
@@ -0,0 +1,12 @@
+#include "ros/ros.h"
+
+/**
+ * This node is our camera node. We will be using it for retrieving raw input from our D-435 camera, applying some machine-learned algorithm to said input and then
+ * feeding the processed data as an output.
+ */
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
